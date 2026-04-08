ecc3fa2271fc5c33c21b1a6129ee9171aa758217
Joaquin Suazo <ja.suazol@alum.up.edu.pe>
2026-04-08 02:18:03 +0000

    Añadir el informe de transparencia al inicio 2.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
         <title>Voto Estratégico - 2026</title>
 </svelte:head>
 <div style="position: fixed; top: 20px; left: 20px; z-index: 50;">
-  <a href="/src/lib/assets/Informe de Transparencia - Voto Estratégico 032026.pdf" target="_blank" rel="noopener noreferrer" 
+	<a href="/Informe%20de%20Transparencia%20-%20Voto%20Estrat%C3%A9gico%20032026.pdf" target="_blank" rel="noopener noreferrer" 
      style="display: inline-block; 
              background-color: black; 
              color: white; 
